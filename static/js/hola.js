5678b3cb52148d5a62a53f7fe2b4ba1fe0235182
AndresE27 <89863261+AndresE27@users.noreply.github.com>
2021-10-06 01:08:13 -0500

    Update hola.js

--- static/js/hola.js
+++ static/js/hola.js
@@ -1,3 +1,7 @@
 alert('Hola desde la PC');
 document.getElementById('sensor').innerHTML='Valor1';
 document.getElementById('sensor2').innerHTML='Valor2';
+
+function LED1_On(){
+  console.log('Se presiono el boton 1')
+}
