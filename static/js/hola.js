f3a38b4732b945b3afd8a1af378ad837eb785ee2
AndresE27 <89863261+AndresE27@users.noreply.github.com>
2021-10-06 01:11:28 -0500

    Update hola.js

--- static/js/hola.js
+++ static/js/hola.js
@@ -3,5 +3,9 @@ document.getElementById('sensor').innerHTML='Valor1';
 document.getElementById('sensor2').innerHTML='Valor2';
 
 function LED1_On(){
-  console.log('Se presiono el boton 1')
+  console.log('Se presiono el boton On')
+}
+
+function LED1_Off(){
+  console.log('Se presiono el boton Off')
 }
