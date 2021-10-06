0ab0b97f9ecba4278cb6531b61b271fa56546403
AndresE27 <89863261+AndresE27@users.noreply.github.com>
2021-10-06 01:18:54 -0500

    Update hola.js

--- static/js/hola.js
+++ static/js/hola.js
@@ -1,11 +1,13 @@
-alert('Hola desde la PC');
-document.getElementById('sensor').innerHTML='Valor1';
-document.getElementById('sensor2').innerHTML='Valor2';
+//alert('Hola desde la PC');
+//document.getElementById('sensor').innerHTML='Valor1';
+//document.getElementById('sensor2').innerHTML='Valor2';
 
 function LED1_On(){
+  document.getElementById('sensor').innerHTML='Led Encendido';
   console.log('Se presiono el boton On')
 }
 
 function LED1_Off(){
+  document.getElementById('sensor').innerHTML='Led Apagado';
   console.log('Se presiono el boton Off')
 }
